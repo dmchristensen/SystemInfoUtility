dbcf2facca4aa32ca367805b68769d2c1acb9757
Darryl Christensen <darryl.m.christensen@gmail.com>
2022-04-18 09:18:52 -0600

    Addm assembly version number to UI

---
+++ src/SystemInfoUtility/ViewModels/ShellWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+using System.Reflection;
+
+namespace SystemInfoUtility.ViewModels
+{
+    public class ShellWindowViewModel : BindableBase
+    {
+        private string _applicationVerion = default!;
+
+        public string ApplicationVersion
+        {
+            get { return _applicationVerion; }
+            set { SetProperty(ref _applicationVerion, value); }
+        }
+
+        public ShellWindowViewModel()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            ApplicationVersion = $"v{assembly.GetName().Version.ToString()}";
+        }
+    }
+}
