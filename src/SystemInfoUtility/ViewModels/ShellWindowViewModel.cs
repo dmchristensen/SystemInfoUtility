978b2c34515ee3bbf82abe932e214e7bfdf233a3
Darryl Christensen <darryl.m.christensen@gmail.com>
2022-04-18 09:30:09 -0600

    Add base AuotUpdater.NET version check

--- src/SystemInfoUtility/ViewModels/ShellWindowViewModel.cs
+++ src/SystemInfoUtility/ViewModels/ShellWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using Prism.Mvvm;
 using System.Reflection;
+using AutoUpdaterDotNET;
+using System.Windows.Threading;
+using System;
 
 namespace SystemInfoUtility.ViewModels
 {
     public class ShellWindowViewModel : BindableBase
     {
+        private readonly DispatcherTimer _updateTimer;
         private string _applicationVerion = default!;
 
         public string ApplicationVersion
@@ -17,6 +21,21 @@ namespace SystemInfoUtility.ViewModels
         {
             var assembly = Assembly.GetEntryAssembly();
             ApplicationVersion = $"v{assembly.GetName().Version.ToString()}";
+
+            AutoUpdater.LetUserSelectRemindLater = true;
+            AutoUpdater.RemindLaterTimeSpan = RemindLaterFormat.Minutes;
+            AutoUpdater.RemindLaterAt = 1;
+            AutoUpdater.ReportErrors = true;
+
+            _updateTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(30) };
+
+            _updateTimer.Tick += _updateTimer_Tick;
+            _updateTimer.Start();
+        }
+
+        private void _updateTimer_Tick(object? sender, EventArgs e)
+        {
+            AutoUpdater.Start(@"https://qa.stratumglobal.com/update-wpf/version.xml");
         }
     }
 }
