15e161fae6e387f3d9f70e2da419277197d9fd02
Darryl Christensen <darryl.m.christensen@gmail.com>
2022-05-16 13:29:41 -0600

    Use appsettings for update server

--- src/SystemInfoUtility/App.xaml.cs
+++ src/SystemInfoUtility/App.xaml.cs
@@ -9,6 +9,8 @@ using SystemInfoUtility.Content;
 using SystemInfoUtility.Navigation;
 using SystemInfoUtility.Regions;
 using SystemInfoUtility.Views;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace SystemInfoUtility
 {
@@ -30,7 +32,7 @@ namespace SystemInfoUtility
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-
+            containerRegistry.RegisterInstance(typeof(IConfiguration), AddConfiguration());
         }
 
         protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
@@ -41,14 +43,29 @@ namespace SystemInfoUtility
 
         protected override void OnInitialized()
         {
+            var configuration = Container.Resolve<IConfiguration>();
+            var serverAddress = configuration.GetValue<string>("ServerAddress");
+
             AutoUpdater.LetUserSelectRemindLater = true;
             AutoUpdater.RemindLaterTimeSpan = RemindLaterFormat.Minutes;
             AutoUpdater.RemindLaterAt = 1;
             AutoUpdater.ReportErrors = false;
 
-            AutoUpdater.Start(@"https://qa.stratumglobal.com/update-wpf/version.xml");
+            AutoUpdater.Start(@$"{serverAddress}/version.xml");
 
             base.OnInitialized();
+        }
+
+        private IConfiguration AddConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+#if DEBUG
+            builder.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+#endif
+            return builder.Build();
         }
     }
 }
